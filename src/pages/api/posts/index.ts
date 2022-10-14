1fcfed41fc66ae0d84ad79e375a7006416671179
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-15 01:12:46 +0300

    /posts endpoint created

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,12 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-type Data = {
-  name: string;
-};
+import firestore from '../../../services/firestore';
+import { Post } from '../../../types/Post';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Post[]>,
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  try {
+    let posts;
+    const fields = req.query.fields as string;
+    const result: Post[] = [];
+
+    if (fields) {
+      const fieldArray = fields.split(',');
+      posts = await firestore.collection('posts').orderBy('creationDate', 'desc').select(...fieldArray).get();
+    } else {
+      posts = await firestore.collection('posts').orderBy('creationDate', 'desc').get();
+    }
+
+    posts.forEach((post) => {
+      const data = post.data() as Post;
+      result.push({ ...data, id: post.id });
+    });
+
+    res.status(200).send(result);
+  } catch {
+    res.status(500).send([]);
+  }
 }
