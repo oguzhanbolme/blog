98d28dfa0546d89e1038c4c0790f8e8d79ea1f25
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-16 00:34:50 +0300

    _app.tsx updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../assets/css/global.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '../context/Theme';
+import Navbar from '../layouts/Navbar';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp;
