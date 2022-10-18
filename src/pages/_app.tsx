800f2909bac60d1cb29087f9ba7ceadd04ae306c
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-18 18:38:15 +0300

    _app.tsx updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import '../assets/css/global.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '../context/Theme';
+import { ThemeProvider } from 'next-themes';
 import Navbar from '../layouts/Navbar';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Navbar />
-      <Component {...pageProps} />
+    <ThemeProvider enableSystem defaultTheme="dark" attribute="class">
+      <div className="min-h-screen bg-white text-[#101827] dark:bg-[#101827] dark:text-white">
+        <div className="max-w-4xl mx-auto">
+          <div className="py-6 px-2">
+            <Navbar />
+          </div>
+          <div className="px-4">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
