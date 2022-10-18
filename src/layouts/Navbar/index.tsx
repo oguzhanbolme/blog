b91f3c6058057c277b0dab5c1c6cac07ee4fd678
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-18 20:29:12 +0300

    navbar updated

---
+++ src/layouts/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import LanguageSelection from './components/LanguageSelection';
+import ThemeSelection from './components/ThemeSelection';
+
+export default function Navbar() {
+  const { locale } = useRouter();
+
+  return (
+    <div className="flex justify-between">
+      <Link href="/" locale={locale}>
+        <span className="text-2xl text-white p-2 cursor-pointer rounded-lg bg-gradient-to-br from-purple-600 to-blue-500">
+          Oğuzhan Bölme Blog
+        </span>
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <LanguageSelection />
+        <ThemeSelection />
+      </div>
+    </div>
+  );
+}
