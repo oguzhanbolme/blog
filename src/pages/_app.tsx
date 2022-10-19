e994ccde4ff4e57349a7ce8a2b147a4103fa0fa0
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-19 22:25:11 +0300

    _app.tsx updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,23 @@ import '../assets/css/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Navbar from '../layouts/Navbar';
+import { SearchProvider } from '../context/Search';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem defaultTheme="dark" attribute="class">
-      <div className="min-h-screen bg-white text-[#101827] dark:bg-[#101827] dark:text-white">
-        <div className="max-w-4xl mx-auto">
-          <div className="py-6 px-2">
-            <Navbar />
-          </div>
-          <div className="px-4">
-            <Component {...pageProps} />
+      <SearchProvider>
+        <div className="min-h-screen bg-white text-[#101827] dark:bg-[#101827] dark:text-white">
+          <div className="max-w-4xl mx-auto">
+            <header className="px-2 py-6">
+              <Navbar />
+            </header>
+            <main className="px-5 pt-2 md:px-10 md:pt-4">
+              <Component {...pageProps} />
+            </main>
           </div>
         </div>
-      </div>
+      </SearchProvider>
     </ThemeProvider>
   );
 }
