111c521d9cb9a70aae511ade8e80a164d4a82b6d
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-19 22:28:23 +0300

    useTimeout hook created

---
+++ src/hooks/useTimeout.ts
@@ -0,0 +1,32 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export default function useTimeout(callback: Function, delay: number) {
+  const callbackRef = useRef(callback);
+  const timeoutRef: any = useRef();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const set = useCallback(() => {
+    timeoutRef.current = setTimeout(() => callbackRef.current(), delay);
+  }, [delay]);
+
+  const clear = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    set();
+    return clear;
+  }, [delay, set, clear]);
+
+  const reset = useCallback(() => {
+    clear();
+    set();
+  }, [clear, set]);
+
+  return { clear, reset };
+}
