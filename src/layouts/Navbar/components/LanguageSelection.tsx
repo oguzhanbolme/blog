e9459d89d1e074e9525d765fb5ba719d02e6516f
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-19 22:27:00 +0300

    language selection updated

--- src/layouts/Navbar/components/LanguageSelection.tsx
+++ src/layouts/Navbar/components/LanguageSelection.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import { mdiTranslate } from '@mdi/js';
 import Icon from '@mdi/react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -11,7 +14,7 @@ export default function LanguageSelection() {
   const { reset } = useTimeout(() => setDropdownVisibility(false), 250);
 
   return (
-    <div>
+    <div className="flex">
       <button
         type="submit"
         className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
@@ -26,17 +29,20 @@ export default function LanguageSelection() {
       <div
         className={`${
           !dropdownVisibility && 'hidden'
-        } z-10 w-28 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 absolute -ml-20`}
+        } z-10 w-28 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 absolute -ml-20 mt-10`}
       >
         <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
           {locales?.map((localeItem) => (
             <Link key={localeItem} href={asPath} locale={localeItem}>
-              <span
-                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
-                aria-hidden="true"
-              >
-                {localeItem.toUpperCase()}
-              </span>
+              <div className="flex items-center gap-2 py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+                <Image
+                  height="24"
+                  width="24"
+                  src={require(`../../../assets/images/flags/${localeItem.toLowerCase()}.png`)}
+                  alt={localeItem}
+                />
+                <span>{localeItem.toUpperCase()}</span>
+              </div>
             </Link>
           ))}
         </ul>
