4d98d13731ff4b0e7ced7154dafbfa82a33ef5f7
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-19 22:26:35 +0300

    theme selection updated

--- src/layouts/Navbar/components/ThemeSelection.tsx
+++ src/layouts/Navbar/components/ThemeSelection.tsx
@@ -6,7 +6,7 @@ export default function ThemeSelection() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div>
+    <div className="flex">
       <button
         type="submit"
         className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
