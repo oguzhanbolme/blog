63804ad7da5500c5a39a51a2b70356781a9a700e
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-20 00:46:13 +0300

    convertDateToString util updated

--- src/utils/convertDateToString.ts
+++ src/utils/convertDateToString.ts
@@ -1,7 +1,10 @@
+import { useRouter } from 'next/router';
 import 'dayjs/locale/en';
 import 'dayjs/locale/tr';
 import dayjs from 'dayjs';
 
-export default function convertDateToString(date: string, locale: string = 'en') {
-  return dayjs(date).locale(locale).format('MMMM DD YYYY');
+export default function convertDateToString(date: string) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { locale } = useRouter();
+  return dayjs(date).locale(locale || 'en').format('MMMM DD YYYY');
 }
