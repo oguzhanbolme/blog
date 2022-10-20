d6df39e4fb75951196695f8dadca424574d13d08
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-20 18:46:33 +0300

    post type updated

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,10 +1,11 @@
 import { DynamicContent } from './DynamicContent';
+import { Tag } from './Tag';
 
 export type Post = {
   id: string;
   content: DynamicContent;
   creationDate: string;
   description: DynamicContent;
-  tags: string[];
+  tags: Tag[];
   title: DynamicContent;
 };
