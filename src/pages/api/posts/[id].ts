17aa5e54ff49332111f41e5b9bfca2cf06fbce6c
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-22 20:54:30 +0300

    /api/post/id endpoint updated

--- src/pages/api/posts/[id].ts
+++ src/pages/api/posts/[id].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import firestore from '../../../services/firestore';
 import { Post } from '../../../types/Post';
+import { Tag } from '../../../types/Tag';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,7 +10,19 @@ export default async function handler(
   try {
     const id = req.query.id as string;
     const post = await firestore.collection('posts').doc(id).get();
-    const result = { ...post.data(), id } as Post;
+    let result = { ...post.data(), id } as Post;
+
+    const tags: Tag[] = await (await fetch(`${process.env.DOMAIN_URL}/api/tags`)).json();
+
+    const data = post.data() as Post;
+    const matchedTags: Tag[] = [];
+
+    for (let i = 0; i < data.tags.length; i += 1) {
+      const matchedTag = tags.find((tag) => tag.id === (data.tags[i] as unknown as string));
+      if (matchedTag) matchedTags.push(matchedTag);
+    }
+
+    result = { ...result, tags: matchedTags };
 
     res.status(200).send(result);
   } catch {
