edda26dff27c2d061c455db5606c49f5d163bf9c
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-24 19:30:33 +0300

    .env and gitignore updated

--- src/services/firestore/index.ts
+++ src/services/firestore/index.ts
@@ -2,5 +2,8 @@ import { Firestore } from '@google-cloud/firestore';
 
 export default new Firestore({
   projectId: process.env.PROJECT_ID,
-  keyFilename: process.env.KEY_FILE_NAME,
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  },
 });
