1b54f2b828496b2cca85c0a0c74512e43cdeae70
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-10-29 23:17:37 +0300

    google analytics setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../assets/css/global.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import { SearchProvider } from '../context/Search';
 import Navbar from '../layouts/Navbar';
@@ -7,15 +8,32 @@ import PageLayout from '../layouts/PageLayout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem defaultTheme="dark" attribute="class">
-      <SearchProvider>
-        <PageLayout>
-          <>
-            <Navbar />
-            <Component {...pageProps} />
-          </>
-        </PageLayout>
-      </SearchProvider>
-    </ThemeProvider>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VLLNWVQWR2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VLLNWVQWR2');
+        `}
+      </Script>
+
+      <ThemeProvider enableSystem defaultTheme="dark" attribute="class">
+        <SearchProvider>
+          <PageLayout>
+            <>
+              <Navbar />
+              <Component {...pageProps} />
+            </>
+          </PageLayout>
+        </SearchProvider>
+      </ThemeProvider>
+    </>
   );
 }
